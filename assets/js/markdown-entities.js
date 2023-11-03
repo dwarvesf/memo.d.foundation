d854407690a2ed647b01d2a9c965ae36e2723efe
Tom X Nguyen <tom81094@gmail.com>
2023-11-03 09:31:43 +0000

    fix: allow github markdown-like blockquoting

--- assets/js/markdown-entities.js
+++ assets/js/markdown-entities.js
@@ -1,13 +1,49 @@
 const MARKDOWN_BOLD_PATTERN = /^\*\*.*\*\*$/;
+const MARKDOWN_BLOCKQUOTE_PATTERN = /(&gt;)+(.*)/;
 
-const selectors = ['code', 'span', 'a'];
+const boldPatternSelectors = ['code', 'span', 'a'];
 
-selectors.forEach(selector => {
+boldPatternSelectors.forEach(selector => {
     Array.from(document.querySelectorAll(selector) || []).forEach(element => {
         if (MARKDOWN_BOLD_PATTERN.test(element.innerHTML.trim())) {
             const codeWithBold = element.innerHTML.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
 
             element.innerHTML = codeWithBold;
+        }
+    })
+})
+
+const blockquotePatternSelectors = ['p'];
+
+blockquotePatternSelectors.forEach(selector => {
+    Array.from(document.querySelectorAll(selector) || []).forEach(element => {
+        if (MARKDOWN_BLOCKQUOTE_PATTERN.test(element.innerHTML.trim())) {
+            const quotes = element.innerHTML.split(/^&gt;(?<=(&gt;|[A-Za-z]))/gm);
+            const quotesFlatNewline = quotes.flatMap(quote => quote.split('\n').filter(el => el));
+            const flatQuotes = quotesFlatNewline.map(quote =>
+                quote.replace(/^(&gt;)+(.+)/gm, '$2')
+            );
+
+            let depth = 0;
+            let unnest = false;
+            const codeWithBlockquotes = flatQuotes.reduce((acc, curr) => {
+                if (curr === '&gt;') {
+                    acc += '<blockquote>'
+                    depth += 1;
+                    unnest = false;
+                    return acc;
+                }
+                if (unnest) {
+                    [...Array(depth)].forEach(() => {
+                        acc += '</blockquote>'
+                    });
+                }
+                acc += `<p>${curr}</p>`
+                unnest = true;
+                return acc;
+            }, '')
+
+            element.innerHTML = codeWithBlockquotes;
         }
     })
 })
