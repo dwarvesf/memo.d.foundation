663ef31751cb32d43b75c8cd243988adf19ee25f
Tom X Nguyen <tom81094@gmail.com>
2023-11-10 05:01:23 +0700

    fix: set default keygen settings and remove email prompt

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -6,8 +6,7 @@ if [ -f ~/.ssh/id_rsa.pub ]; then
 else
   echo "You don't have an ssh key. Generating one for you."
   # Generate an ssh key with the user's email as the comment
-  read -p "Enter your email: " email
-  ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa -N ""
+  ssh-keygen -f ~/.ssh/id_rsa -N ""
 fi
 
 # Output the public key
