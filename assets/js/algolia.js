22c5214e15a3df06a1869aa28185cf8218c9ac76
Tom X Nguyen <tom81094@gmail.com>
2023-11-21 16:33:39 +0700

    chore: trim whitespace

--- assets/js/algolia.js
+++ assets/js/algolia.js
@@ -2,7 +2,7 @@ import * as params from '@params';
 
 window.docsearch({
   container: '#docsearch',
-  appId: params.application_id,
+  appId: params.application_id.trim(),
   indexName: 'note.d.foundation',
-  apiKey: params.api_key,
+  apiKey: params.api_key.trim(),
 });
