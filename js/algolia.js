36c79ef5405337b5cb4b5ffb66ee692853a0f021
monotykamary <monotykamary@users.noreply.github.com>
2023-11-21 09:34:22 +0000

    deploy: 7fac900be9e790f2bb8842e28e9c766348e1c107

--- js/algolia.js
+++ js/algolia.js
@@ -1,13 +1,13 @@
 (() => {
   // ns-params:@params
-  var api_key = "310d8f7559c289385c624e31e65543b3 ";
+  var api_key = "310d8f7559c289385c624e31e65543b3";
   var application_id = "B0BWKXLVM9";
 
   // <stdin>
   window.docsearch({
     container: "#docsearch",
-    appId: application_id,
+    appId: application_id.trim(),
     indexName: "note.d.foundation",
-    apiKey: api_key
+    apiKey: api_key.trim()
   });
 })();
