95d0b978586b1b351e62489a1f24fbb92343c00a
Tom X Nguyen <tom81094@gmail.com>
2023-11-29 10:46:53 +0700

    feat: parse by clause

--- scripts/convert_dataview_to_sql.py
+++ scripts/convert_dataview_to_sql.py
@@ -1,4 +1,5 @@
 from pyparsing import *
+import json
 
 # Define punctuation and keywords
 semicolon = Combine(Literal(';') + lineEnd)
@@ -14,10 +15,19 @@ list_kw = Keyword('LIST', caseless=True)
 task_kw = Keyword('TASK', caseless=True)
 table_kw = Keyword('TABLE', caseless=True)
 from_kw = Keyword('FROM', caseless=True)
+
 and_kw = Keyword('AND', caseless=True)
 not_kw = Keyword('NOT', caseless=True)
 or_kw = Keyword('OR', caseless=True)
 as_kw = Keyword('AS', caseless=True)
+
+where_kw = Keyword('WHERE', caseless=True)
+sort_kw = Keyword('SORT', caseless=True)
+limit_kw = Keyword('LIMIT', caseless=True)
+group_kw = Keyword('GROUP BY', caseless=True)
+
+asc_kw = Keyword('ASC', caseless=True)
+desc_kw = Keyword('DESC', caseless=True)
 
 reserved_words = Or(keyword_objects)
 
@@ -27,8 +37,8 @@ wikilink = Combine(Optional(lparen) + lbracket + lbracket + delimitedList(Word(a
 
 ident = ~reserved_words + (common_word + Optional(wikilink) | dblQuotedString.setParseAction(removeQuotes) | sglQuotedString.setParseAction(removeQuotes))
 table = Forward()
-table_delim = and_kw | rparen + or_kw + lparen | or_kw
-table_list = delimitedList(table, delim=table_delim)
+nested_delim = and_kw | rparen + or_kw + lparen | or_kw
+table_list = delimitedList(table, delim=nested_delim)
 
 simple_table = Combine(Optional(Literal('!')) + ident)
 nested_table = lparen.suppress() + table_list + rparen.suppress()
@@ -41,11 +51,36 @@ column_list = delimitedList(ident, delim=column_delim)
 
 # Define clauses
 from_clause = from_kw.suppress() + table_list
-table_clause = (list_kw.suppress() | table_kw.suppress() | task_kw.suppress()) + column_list
+column_clause = (list_kw.suppress() | table_kw.suppress() | task_kw.suppress()) + column_list
+where_clause = where_kw.suppress() + delimitedList(ident, delim=nested_delim)
+sort_clause = sort_kw.suppress() + delimitedList(ident, delim=comma) + (asc_kw | desc_kw)
+limit_clause = limit_kw.suppress() + Word(nums)
+group_clause = group_kw.suppress() + delimitedList(ident, delim=comma)
+
+clauses = {
+	"from": from_clause,
+	"select": column_clause,
+	"where": where_clause,
+	"sort": sort_clause,
+	"limit": limit_clause,
+	"groupBy": group_clause,
+}
 
 # Test the parser
-txt = 'FROM #status/open OR #status/wip'
-for token, start, end in table_clause.scanString(txt):
-  print("Columns:", token.asList())
-for token, start, end in from_clause.scanString(txt):
-  print("Tables:", token.asList())
+txt = '''
+TABLE feat
+FROM earn
+WHERE !completed
+SORT created ASC
+LIMIT 10
+GROUP BY file.link
+SORT rows.file.ctime ASC
+'''
+
+query_dict = {key: [] for key in clauses.keys()}
+
+for key, clause in clauses.items():
+	for token, start, end in clause.scanString(txt):
+		query_dict[key].append(token.asList())
+
+print(json.dumps(query_dict, indent=2))
