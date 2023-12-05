1ce5a0c7723cdf43fceee5a9bec4586c56d5a764
monotykamary <monotykamary@users.noreply.github.com>
2023-12-05 16:11:58 +0000

    deploy: 940e7e69ec15c10d0be647fc37dda4e5119c7642

--- js/process-comments.js
+++ js/process-comments.js
@@ -3,27 +3,36 @@ const nodeIterator = document.createNodeIterator(
   NodeFilter.SHOW_COMMENT
 );
 
-const pairs = [];
-const buffer = [];
+const pairs = new Map();
 
 while(nodeIterator.nextNode()) {
   const commentNode = nodeIterator.referenceNode;
   const name = commentNode.data.trim();
-  buffer.push(commentNode);
+  const splitName = name.split("/")[1];
+  let pairValue = pairs.get(name);
+  console.log(name);
 
-  if (buffer.length > 1) {
-    const prevName = buffer[0].data.trim();
-    if (name.includes(prevName)) {
-      pairs.push([...buffer]);
+  if (splitName) {
+    pairValue = pairs.get(splitName);
+    pairs.set(splitName, [...pairValue, commentNode]);
+  }
+  else {
+    if (!pairValue) {
+      pairs.set(name, [commentNode]);
     }
-    buffer.length = 0;
+    else {
+      pairs.set(name, [...pairValue, commentNode]);
+    }
   }
 }
 
+console.log(pairs);
 pairs.forEach(([start, end]) => {
   if (start.nodeType === Node.COMMENT_NODE) {
     const div = document.createElement("div");
-    div.classList.add(start.data.trim());
+    const className = start.data.trim().split(" ")[0];
+
+    div.classList.add(className);
     start.parentNode.replaceChild(div, start);
     start = div;
   }
