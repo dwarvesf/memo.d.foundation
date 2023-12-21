10368371c512488b697a96c21f95864c39f2f388
Tom X Nguyen <tom81094@gmail.com>
2023-12-21 22:03:28 +0700

    fix: make it functional

--- assets/js/table-of-contents.js
+++ assets/js/table-of-contents.js
@@ -1,52 +1,36 @@
-class Scroller {
-	static init() {
-		if (document.querySelector('.pagenav')) {
-			let i = 0;
-			this.pagenav = document.querySelectorAll('.pagenav a');
-			this.pagenav.forEach(link => {
-				if (i === 0) {
-					link.classList.add('text-active');
-					i++;
-				}
-				link.classList.add('transition', 'duration-200');
-			});
-			this.headers = Array.from(this.pagenav).map(link => {
-				return document.querySelector(`#${link.href.split('#')[1]}`);
-			});
-			this.ticking = false;
-			window.addEventListener('scroll', (e) => {
-				this.onScroll()
-			});
-		}
-	}
+const initScroller = () => {
+	const pagenav = document.querySelectorAll('.pagenav a');
+	const headers = Array.from(pagenav).map(link => document.querySelector(`#${link.href.split('#')[1]}`));
 
-	static onScroll() {
-		if (!this.ticking) {
-			requestAnimationFrame(this.update.bind(this));
-			this.ticking = true;
-		}
-	}
+	const addClassToFirstLink = () => pagenav[0].classList.add('text-active');
+	const addTransitionClasses = () => pagenav.forEach(link => link.classList.add('transition', 'duration-200'));
 
-	static update() {
-		this.activeHeader ||= this.headers[0];
-		let activeIndex = this.headers.findIndex((header) => {
-			return header.getBoundingClientRect().top > 160;
-		});
-		if (activeIndex == -1) {
-			activeIndex = this.headers.length - 1;
-		} else if (activeIndex > 0) {
-			activeIndex--;
-		}
-		let active = this.headers[activeIndex];
-		if (active !== this.activeHeader) {
-			this.activeHeader = active;
-			this.pagenav.forEach(link => link.classList.remove('text-active'));
-			this.pagenav[activeIndex].classList.add('text-active');
-		}
-		this.ticking = false;
-	}
-}
+	addClassToFirstLink();
+	addTransitionClasses();
 
-document.addEventListener('DOMContentLoaded', function (e) {
-	Scroller.init();
-})
+	const onScroll = () => {
+		if (!ticking) {
+			requestAnimationFrame(update);
+			ticking = true;
+		}
+	};
+
+	const update = () => {
+		const activeHeader = headers.find(header => header.getBoundingClientRect().top > 160) || headers[0];
+		const activeIndex = headers.indexOf(activeHeader);
+
+		if (activeHeader !== currentActiveHeader) {
+			currentActiveHeader = activeHeader;
+			pagenav.forEach(link => link.classList.remove('text-active'));
+			pagenav[activeIndex].classList.add('text-active');
+		}
+		ticking = false;
+	};
+
+	let ticking = false;
+	let currentActiveHeader = headers[0];
+
+	window.addEventListener('scroll', onScroll);
+};
+
+document.addEventListener('DOMContentLoaded', initScroller);
