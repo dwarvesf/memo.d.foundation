ee90773d94bc27e4549ebf9bb463ac2f9d335632
tuanddd <vincenttuandao@gmail.com>
2024-02-28 13:33:31 +0700

    fix: dark/light icon

--- assets/js/dark-light-check.js
+++ assets/js/dark-light-check.js
@@ -2,25 +2,23 @@ const root = document.documentElement;
 
 const runColorMode = (fn) => {
   if (!window.matchMedia) return;
-  
-  const query = window.matchMedia('(prefers-color-scheme: dark)');
+
+  const query = window.matchMedia("(prefers-color-scheme: dark)");
   fn(query.matches);
 
-  query.addEventListener('change', (event) => fn(event.matches));
-}
+  query.addEventListener("change", (event) => fn(event.matches));
+};
 
 runColorMode((isDarkMode) => {
   const preferenceTheme = localStorage.getItem("df.note.theme");
   console.log("preferenceTheme", preferenceTheme);
-  if (preferenceTheme === 'dark' || preferenceTheme === "light") {
+  if (preferenceTheme === "dark" || preferenceTheme === "light") {
     isDarkMode = preferenceTheme === "dark";
   }
 
   if (isDarkMode) {
     root.setAttribute("data-theme", "dark");
-    this._theme = "dark";
   } else {
     root.setAttribute("data-theme", "light");
-    this._theme = "light";
   }
-})
+});
