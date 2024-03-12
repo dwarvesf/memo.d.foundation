4815c351b091f290fd75128a52698fddf778fb85
tuanddd <tuanddd@users.noreply.github.com>
2024-03-12 07:38:58 +0000

    deploy: c7e4ba335b94cbdc59b2ed215986517fca81e651

--- js/reading-mode.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("#btn-reading-mode-toggler");
-const sidebar = document.querySelector("#sidebar");
-
-btn.addEventListener("click", () => {
-  const isReadingMode =
-    document.documentElement.getAttribute("data-reading-mode") === "true";
-  if (isReadingMode) return;
-  sidebar.classList.remove("menu-reading-mode");
-});
