32bd7afa93c84c3cd781a6dfc84b1b6707bbe724
tuanddd <vincenttuandao@gmail.com>
2024-03-13 09:50:14 +0700

    fix: style

--- assets/js/keyboard-shortcuts.js
+++ assets/js/keyboard-shortcuts.js
@@ -1,7 +1,8 @@
-Mousetrap.bind("command+shift+f", function () {
+Mousetrap.bind("command+shift+f", function() {
   Alpine.store("readingMode").toggle();
 });
 
-Mousetrap.bind("escape", function () {
+Mousetrap.bind("escape", function() {
   Alpine.store("sidebar").close();
+  Alpine.store("graphFullScreen").close();
 });
