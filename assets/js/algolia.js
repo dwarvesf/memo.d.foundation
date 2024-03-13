309c67674715f100d13b139e4fa03b6137370c6d
Tom X Nguyen <tom81094@gmail.com>
2024-03-13 10:09:17 +0700

    chore: migrate to memo

--- assets/js/algolia.js
+++ assets/js/algolia.js
@@ -3,7 +3,7 @@ import * as params from '@params';
 window.docsearch({
   container: '#docsearch',
   appId: params.application_id.trim(),
-  indexName: 'note.d.foundation',
+  indexName: 'memo.d.foundation',
   apiKey: params.api_key.trim(),
   placeholder: "Search notes",
 });
