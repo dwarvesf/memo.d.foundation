fc53a5077f328f46744eddc209dfb5a598d5b365
Tom X Nguyen <tom81094@gmail.com>
2024-03-15 12:05:27 +0700

    perf: simplify pull

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Initialize any uninitialized submodules
-git submodule update --init --recursive
+# update submodule branches recursively to main or master
+git submodule foreach --recursive 'git checkout main || git checkout master' 
 
 # Update submodules recursively (in case they're not up-to-date)
 git submodule update --remote --recursive
-
-# update submodule branches recursively to main or master
-git submodule foreach --recursive 'git checkout main || git checkout master' 
