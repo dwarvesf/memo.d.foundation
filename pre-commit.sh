c4cffd3b2744de1cd79e6104e968dda36b04957b
Tom X Nguyen <tom81094@gmail.com>
2024-03-17 03:04:09 +0700

    chore: update to fallback to main

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update submodule branches recursively to branch set on .gitmodules
-git submodule foreach --recursive 'git checkout $(git config -f "$toplevel/.gitmodules" submodule."$(basename `pwd`)"."branch")'
+git submodule foreach --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo main)'
 
 # Update submodules recursively (in case they're not up-to-date)
 git submodule update --remote --recursive
