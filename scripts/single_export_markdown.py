dc0fbaa5aee2e2646ff2498f334184bb3d4d794a
Tom X Nguyen <tom81094@gmail.com>
2024-03-28 11:55:12 +0700

    fix: make the export more robust and cross link files

--- scripts/single_export_markdown.py
+++ scripts/single_export_markdown.py
@@ -2,6 +2,7 @@ import os
 import re
 import urllib.parse
 import sys
+import frontmatter
 
 obsidian_link_regex_compiled = re.compile(r"\[\[(.*?)\]\]")
 
@@ -12,8 +13,23 @@ def process_markdown_file(file_path, export_path):
             file_path (str): Path to the Markdown file.
     """
 
+    linked_files = []
     with open(file_path, "r") as file:
         content = file.read()
+
+    frontmatter_properties = frontmatter.loads(content)
+    
+    # check if frontmatter properties exist
+    if not frontmatter_properties.keys():
+        return content, linked_files
+
+    title = frontmatter_properties.get("title", "")
+    description = frontmatter_properties.get("description", "")
+    tags = frontmatter_properties.get("tags", "")
+
+    # if any of the frontmatter properties are empty, exit the function
+    if not all([title, description, tags]):
+        return content, linked_files
 
     file_dir = os.path.dirname(file_path)
     root_dir = file_path.split("/")[0]
@@ -31,13 +47,11 @@ def process_markdown_file(file_path, export_path):
             # get the path of the each file relative to the root directory, but don't include the root directory itself
             files = [os.path.relpath(os.path.join(root, f), root_dir) for f in files]
             
-            # process only markdown files
-            files = [f for f in files if f.endswith(".md")]
-            
             # for each file, check if the source note name string is in the file
             for file in files:
                 if source_note in file:
                     note_path = file
+                    linked_files.append(file)
                     break
             
             # if note_path is not empty
@@ -46,6 +60,9 @@ def process_markdown_file(file_path, export_path):
         else:  # If the note is not found
             return match.group(0)  # Return the original link
 
+        # make the note_path url safe
+        note_path = urllib.parse.quote(note_path)
+
         return f"[{source_name}]({note_path})"
 
     # Update Obsidian links to markdown
@@ -53,7 +70,16 @@ def process_markdown_file(file_path, export_path):
         obsidian_link_regex_compiled, lambda x: replace_link(x), content
     )
     
-    print(content)
+    # move all linked files to the export path if they don't exist in the export path
+    for linked_file in linked_files:
+        linked_file_path = os.path.join(file_path.split("/")[0], linked_file)
+        export_linked_file_path = linked_file_path.replace(
+            file_path.split("/")[0], export_path
+        )
+        if not os.path.exists(export_linked_file_path):
+            os.makedirs(os.path.dirname(export_linked_file_path), exist_ok=True)
+            shutil.copy2(linked_file_path, export_linked_file_path)
+
     # create a new file matching the directory structure of the file_path to the export_path using the new content
     export_file_path = file_path.replace(file_path.split("/")[0], export_path)
     os.makedirs(os.path.dirname(export_file_path), exist_ok=True)
