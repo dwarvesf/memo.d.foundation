f4ea7efdf451bc3659f0a313c3402f2c2eb29a8d
Tom X Nguyen <tom81094@gmail.com>
2024-04-10 15:02:34 +0700

    perf: update settings script to allow more workers for submodule fetch

--- git-fetch.sh
+++ git-fetch.sh
@@ -6,6 +6,5 @@ git submodule update --init --recursive --filter=blob:none
 # Loop over the submodules and checkout them to the specified branch if they're not set
 git submodule foreach --recursive 'branch=$(git rev-parse --abbrev-ref HEAD); if [ "$(git config --get branch.$branch.remote)" = "" ]; then git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo main); fi'
 
-
 # Loop over the submodules and fetch new changes for each submodule
 git fetch --recurse-submodules
