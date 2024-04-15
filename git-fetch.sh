592ab824ae70daf145966a3b6b896e74f1a28cf3
Tom X Nguyen <tom81094@gmail.com>
2024-04-15 19:12:18 +0700

    chore: add logs and add submodule updates to pre-commit

--- git-fetch.sh
+++ git-fetch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Stash all unstaged changes and pull
+echo "Stashing unstaged changes and pulling..."
 git stash -u
 yes | git pull
 git stash pop
@@ -10,4 +11,5 @@ echo "Updating submodules..."
 yes | git submodule update --init --recursive --remote --progress --merge --filter=blob:none
 
 # Loop over the submodules and checkout them to the specified branch if they're not set
+echo "Checking out submodules..."
 git submodule foreach --recursive 'branch=$(git rev-parse --abbrev-ref HEAD); if [ "$(git config --get branch.$branch.remote)" = "" ]; then git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo main); fi'
