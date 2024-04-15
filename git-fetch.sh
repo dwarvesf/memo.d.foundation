1a303a630312078a5da3973b1ee5852d33960f11
Tom X Nguyen <tom81094@gmail.com>
2024-04-15 19:03:57 +0700

    fix: also add pulling main repo

--- git-fetch.sh
+++ git-fetch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Stash all unstaged changes and pull
+git stash -u
+yes | git pull
+git stash pop
+
 # Loop over the submodules and update them if they're not initialized
-yes | git submodule update --init --recursive --merge --filter=blob:none
+echo "Updating submodules..."
+yes | git submodule update --init --recursive --remote --progress --merge --filter=blob:none
 
 # Loop over the submodules and checkout them to the specified branch if they're not set
 git submodule foreach --recursive 'branch=$(git rev-parse --abbrev-ref HEAD); if [ "$(git config --get branch.$branch.remote)" = "" ]; then git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo main); fi'
-
-# Loop over the submodules and fetch new changes for each submodule
-git fetch --recurse-submodules
