b826ec9a18a8d2a45d715dde4f91487246e855eb
Tom X Nguyen <tom81094@gmail.com>
2024-07-08 15:02:40 +0700

    chore: update duckdb-wasm

--- assets/js/query-engine.mjs
+++ assets/js/query-engine.mjs
@@ -1,4 +1,4 @@
-import * as duckdbduckdbWasm from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev218.0/+esm";
+import * as duckdbduckdbWasm from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev234.0/+esm";
 import { pipeline, env } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1';
 
 window.duckdbduckdbWasm = duckdbduckdbWasm;
@@ -56,7 +56,7 @@ queueMicrotask(async () => {
 })
 
 const getJsDelivrBundles = () => {
-  const jsdelivr_dist_url = `https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev218.0/dist/`;
+  const jsdelivr_dist_url = `https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev234.0/dist/`;
   return {
     mvp: {
       mainModule: `${jsdelivr_dist_url}duckdb-mvp.wasm`,
