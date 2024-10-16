c321c7e7cf1353cc9816ff005e04ab412f670d6a
Tom X Nguyen <tom81094@gmail.com>
2024-10-16 11:55:46 +0700

    feat: allow syncing images

---
+++ lib/memo/common/link_transformer.ex
@@ -0,0 +1,64 @@
+defmodule Memo.Common.LinkTransformer do
+  @moduledoc """
+  A module for transforming local links in markdown content to be compatible with Hashnode.
+  """
+
+  alias Memo.Common.{Slugify, LinkUtils}
+
+  def transform_local_links(content, file, vaultpath) do
+    content
+    |> String.split("\n")
+    |> Enum.map(&(transform_line(&1, file, vaultpath)))
+    |> Enum.join("\n")
+  end
+
+  defp transform_line(line, file, vaultpath) do
+    cond do
+      String.contains?(line, "![") and String.contains?(line, "](") ->
+        transform_image_link(line, file, vaultpath)
+      String.contains?(line, "[") and String.contains?(line, "](") ->
+        transform_markdown_link(line, file, vaultpath)
+      true ->
+        line
+    end
+  end
+
+  defp transform_image_link(line, file, vaultpath) do
+    Regex.replace(~r/!\[([^\]]*)\]\(([^)]+)\)/, line, fn _, alt, link ->
+      transformed_link = transform_link(link, file, vaultpath)
+      "![#{alt}](#{transformed_link})"
+    end)
+  end
+
+  defp transform_markdown_link(line, file, vaultpath) do
+    Regex.replace(~r/\[([^\]]+)\]\(([^)]+)\)/, line, fn _, text, link ->
+      transformed_link = transform_link(link, file, vaultpath)
+      "[#{text}](#{transformed_link})"
+    end)
+  end
+
+  defp transform_link(link, file, vaultpath) do
+    if LinkUtils.is_url?(link) do
+      link
+    else
+      file_dir = Path.dirname(file)
+      absolute_path = Path.expand(link, file_dir)
+      relative_to_vault = Path.relative_to(absolute_path, vaultpath)
+
+      if String.starts_with?(relative_to_vault, "..") do
+        # The link points outside the vault, don't transform it
+        link
+      else
+        path_parts = Path.split(relative_to_vault)
+        filename = List.last(path_parts)
+        directory = path_parts |> Enum.drop(-1) |> Enum.map(&Slugify.slugify/1)
+
+        slugified_path =
+          (directory ++ [Slugify.slugify_filename(filename)])
+          |> Path.join()
+
+        "https://memo.d.foundation/#{slugified_path}"
+      end
+    end
+  end
+end
--- lib/memo/sync_hashnode.ex
+++ lib/memo/sync_hashnode.ex
@@ -1,26 +1,29 @@
 defmodule Memo.SyncHashnode do
   @moduledoc """
   A module to sync markdown files with Hashnode API.
+  This module now includes functionality to transform local links and images
+  before sending content to Hashnode.
   """
 
-  alias Memo.Common.{FileUtils, Frontmatter}
+  alias Memo.Common.{FileUtils, Frontmatter, Slugify, LinkTransformer}
   require Logger
 
   @hashnode_api_url "https://gql.hashnode.com/"
+  @debug_mode true
 
   def run(vaultpath) do
     if File.exists?(".env") do
       DotenvParser.load_file(".env")
     end
 
-    vaultpath = vaultpath || "vault"
+    vaultpath = Path.expand(vaultpath || "vault")
     ignored_patterns = FileUtils.read_export_ignore_file(Path.join(vaultpath, ".export-ignore"))
 
     FileUtils.list_files_recursive(vaultpath)
     |> Enum.filter(&(not FileUtils.ignored?(&1, ignored_patterns, vaultpath)))
     |> Enum.filter(&File.regular?/1)
     |> Enum.filter(&has_hashnode_sync?/1)
-    |> Enum.each(&process_file(&1))
+    |> Enum.each(&process_file(&1, vaultpath))
   end
 
   defp has_hashnode_sync?(file) do
@@ -33,17 +36,27 @@ defmodule Memo.SyncHashnode do
     end
   end
 
-  defp process_file(file) do
+  defp process_file(file, vaultpath) do
     with {:ok, content} <- File.read(file),
          {:ok, frontmatter} <- Frontmatter.parse_frontmatter(content),
          true <- Frontmatter.contains_required_frontmatter_keys?(file) do
       hashnode_meta = Map.get(frontmatter, "hashnode_meta")
       content_without_frontmatter = Frontmatter.strip_frontmatter(content)
+      transformed_content = LinkTransformer.transform_local_links(content_without_frontmatter, file, vaultpath)
 
-      if is_nil(hashnode_meta) do
-        publish_post(file, frontmatter, content_without_frontmatter)
-      else
-        update_post(file, frontmatter, content_without_frontmatter, hashnode_meta)
+      if @debug_mode do
+        IO.puts("Original content:")
+        IO.puts(content_without_frontmatter)
+        IO.puts("\nTransformed content:")
+        IO.puts(transformed_content)
+      end
+
+      unless @debug_mode do
+        if is_nil(hashnode_meta) do
+          publish_post(file, frontmatter, transformed_content)
+        else
+          update_post(file, frontmatter, transformed_content, hashnode_meta)
+        end
       end
     else
       _ ->
