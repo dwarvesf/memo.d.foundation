db52fef44455a8c6e8a10d9e6849748dd5e7da6b
Tom X Nguyen <tom81094@gmail.com>
2025-03-07 18:31:41 +0700

    feat: add markdown plugins

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
-
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
