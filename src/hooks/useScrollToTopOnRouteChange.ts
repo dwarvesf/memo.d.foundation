55ffe15755cf8dd6306faafde00ca003921819fe
Thanh Hai <hthai2201@gmail.com>
2025-04-02 13:43:49 +0700

    fix: disable scroll animation

--- src/hooks/useScrollToTopOnRouteChange.ts
+++ src/hooks/useScrollToTopOnRouteChange.ts
@@ -16,7 +16,6 @@ export function useScrollToTopOnRouteChange(
       if (elementRef.current) {
         elementRef.current.scrollTo({
           top: 0,
-          behavior: 'smooth',
         });
       }
     };
