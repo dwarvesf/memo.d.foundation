28ff161764b50e6fa113318c4f0848ccec32d75f
Thanh Hai <hthai2201@gmail.com>
2025-04-10 11:19:12 +0700

    fix: style

--- src/contexts/Web3Provider.tsx
+++ src/contexts/Web3Provider.tsx
@@ -29,23 +29,30 @@ export const Web3Provider = ({ children }: PropsWithChildren) => {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider
+          options={{ embedGoogleFonts: true }}
           customTheme={{
-            '--ck-font-family': 'var(--font-monospace)',
+            '--ck-font-family': 'var(--font-sans)',
             '--ck-primary-button-background': 'var(--muted)',
             '--ck-primary-button-hover-background': 'var(--border)',
             '--ck-primary-button-color': 'var(--muted-foreground)',
             '--ck-primary-button-hover-color': 'var(--muted-foreground)',
+            '--ck-primary-button-font-weight': 400,
+            '--ck-primary-button-border-radius': 'var(--radius)',
             '--ck-body-background': 'var(--background)',
             '--ck-body-color': 'var(--foreground)',
             '--ck-secondary-button-background': 'var(--secondary-background)',
             '--ck-secondary-button-color': 'var(--secondary-foreground)',
             '--ck-border-radius': 'var(--radius)',
-            '--ck-overlay-background': 'rgba(0, 0, 0, 0.4)',
+            '--ck-overlay-background':
+              'color-mix(in oklab, var(--color-black) 50%, transparent)',
             '--ck-body-color-muted': 'var(--muted-foreground)',
             '--ck-body-color-muted-hover': 'var(--foreground)',
             '--ck-body-background-secondary': 'var(--muted)',
             '--ck-body-background-tertiary': 'var(--accent)',
             '--ck-body-border': 'var(--border)',
+            '--ck-body-color-danger': 'var(--primary)',
+            '--ck-modal-box-shadow':
+              '0px 4px 6px -2px var(--tw-shadow-color, #10182808), 0px 12px 16px -4px var(--tw-shadow-color, #10182814)',
           }}
         >
           {children}
