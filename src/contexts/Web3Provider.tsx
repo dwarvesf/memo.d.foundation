59d760074aae18fe4ffe7a9805dae01ffe056938
Thanh Hai <hthai2201@gmail.com>
2025-04-11 15:27:51 +0700

    fix: theme

--- src/contexts/Web3Provider.tsx
+++ src/contexts/Web3Provider.tsx
@@ -3,12 +3,16 @@ import { base, baseSepolia } from 'wagmi/chains';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {
   RainbowKitProvider,
+  Theme,
   darkTheme,
   getDefaultConfig,
+  lightTheme,
 } from '@rainbow-me/rainbowkit';
 import { PropsWithChildren } from 'react';
 import { WALLETCONNECT_PROJECT_ID } from '@/constants/nft';
 import '@rainbow-me/rainbowkit/styles.css';
+import { useThemeContext } from './theme';
+import { merge } from 'lodash';
 
 const config = getDefaultConfig({
   // Your dApps chains
@@ -39,16 +43,64 @@ export const Web3ProviderInner = ({ children }: PropsWithChildren) => {
   });
   return children;
 };
+
+const customBaseTheme: RecursivePartial<Theme> = {
+  blurs: {
+    modalOverlay: 'small',
+  },
+  colors: {
+    accentColor: 'var(--primary)',
+    accentColorForeground: 'var(--primary-foreground)',
+    actionButtonBorder: 'var(--border)',
+    actionButtonBorderMobile: 'var(--border)',
+    actionButtonSecondaryBackground: 'var(--muted)',
+    closeButton: 'var(--muted-foreground)',
+    closeButtonBackground: 'var(--muted)',
+    connectButtonBackground: 'var(--muted)',
+    connectButtonBackgroundError: 'var(--primary)',
+    connectButtonInnerBackground: 'var(--background)',
+    connectButtonText: 'var(--muted-foreground)',
+    connectButtonTextError: 'var(--primary-foreground)',
+    connectionIndicator: 'var(--primary)',
+    error: 'var(--primary)',
+    generalBorder: 'var(--border)',
+    generalBorderDim: 'var(--border)',
+    menuItemBackground: 'var(--muted)',
+    modalBackdrop: 'color-mix(in oklab, var(--color-black) 50%, transparent)',
+    modalBackground: 'var(--background)',
+    modalBorder: 'var(--border)',
+    modalText: 'var(--foreground)',
+    modalTextDim: 'var(--muted-foreground)',
+    modalTextSecondary: 'var(--muted-foreground)',
+    profileAction: 'var(--muted)',
+    profileActionHover: 'var(--accent)',
+    profileForeground: 'var(--foreground)',
+    selectedOptionBorder: 'var(--primary)',
+    standby: 'var(--primary)',
+  },
+  radii: {
+    actionButton: 'var(--radius)',
+    connectButton: 'var(--radius)',
+    menuButton: 'var(--radius)',
+    modal: 'var(--radius)',
+    modalMobile: 'var(--radius)',
+  },
+  fonts: {
+    body: 'var(--font-sans)',
+  },
+};
+
+const customLightTheme = merge(lightTheme(), customBaseTheme, {});
+const customDarkTheme = merge(darkTheme(), customBaseTheme, {});
+
 export const Web3Provider = ({ children }: PropsWithChildren) => {
+  const { theme } = useThemeContext();
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
-          theme={darkTheme({
-            accentColor: 'var(--muted)',
-            accentColorForeground: 'var(--muted-foreground)',
-            fontStack: 'system',
-          })}
+          theme={theme === 'dark' ? customDarkTheme : customLightTheme}
         >
           <Web3ProviderInner>{children}</Web3ProviderInner>
         </RainbowKitProvider>
