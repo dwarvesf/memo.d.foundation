43e69ce798c6670a7256a62e670dac398d883221
Thanh Hai <hthai2201@gmail.com>
2025-05-05 18:02:39 +0700

    feat: apply prose class

--- src/components/RemoteMdxRenderer.tsx
+++ src/components/RemoteMdxRenderer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { type SerializeResult } from 'next-mdx-remote-client/serialize';
 import Link from 'next/link';
 import { formatMemoPath } from './memo/utils';
+import { cn } from '@/lib/utils';
 
 interface Props {
   mdxSource: SerializeResult;
@@ -11,8 +12,31 @@ interface Props {
 const components: MDXComponents = {
   ContributorHeader: props => <span>{props.name}</span>,
   MemoLink: props => {
-    const { filePath, ...rest } = props;
-    return <Link href={formatMemoPath(filePath || '')} {...rest} />;
+    const { filePath, className, ...rest } = props;
+    return (
+      <Link
+        href={formatMemoPath(filePath || '')}
+        className={cn(
+          'line-clamp-3 text-[1.0625rem] -tracking-[0.0125rem] underline transition-colors duration-200 ease-in-out dark:text-neutral-300',
+          className,
+        )}
+        {...rest}
+      />
+    );
+  },
+  wrapper(props) {
+    const { children } = props;
+    return (
+      <div
+        className={cn(
+          'article-content prose dark:prose-dark prose-headings:font-serif prose-headings:font-semibold prose-headings:tracking-tight max-w-none font-serif',
+          'prose-a:text-foreground prose-a:underline prose-a:decoration-link-decoration prose-a:hover:text-primary prose-a:hover:decoration-primary prose-a:font-[inherit]',
+          'prose-table:border prose-img:mt-[var(--element-margin)]',
+        )}
+      >
+        {children}
+      </div>
+    );
   },
 };
 const RemoteMdxRenderer = (props: Props) => {
