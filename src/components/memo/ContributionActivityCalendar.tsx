6f0308dda27f0c6d0eb710f19567a583a952f872
vincent <vincenttuandao@gmail.com>
2025-05-19 16:27:50 +0700

    chore: add tooltip for activity hover

--- src/components/memo/ContributionActivityCalendar.tsx
+++ src/components/memo/ContributionActivityCalendar.tsx
@@ -4,11 +4,20 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '../ui/dropdown';
 import { Button } from '../ui/button';
 import { CalendarIcon } from 'lucide-react';
 import { useState } from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../ui/tooltip';
+import { format } from 'date-fns';
+
 interface ContributionActivityCalendarProps {
   data: Record<
     string,
@@ -30,34 +39,59 @@ function ContributionActivityCalendar(
   );
 
   return (
-    <div className="mx-auto mb-10 flex w-full max-w-3xl flex-col gap-y-2">
-      <div className="flex items-center justify-between">
-        <span className="text-base">Activity</span>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="sm" className="text-xs">
-              <CalendarIcon className="!h-3 !w-3" />
-              Viewing {year}
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {Object.keys(data)
-              .sort((a, b) => b.localeCompare(a))
-              .map(year => (
-                <DropdownMenuItem key={year} onClick={() => setYear(year)}>
-                  {year}
-                </DropdownMenuItem>
-              ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <TooltipProvider>
+      <div className="mx-auto mb-10 flex w-full max-w-3xl flex-col gap-y-2">
+        <div className="flex items-center justify-between">
+          <span className="text-base">Activity</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm" className="text-xs">
+                <CalendarIcon className="!h-3 !w-3" />
+                Viewing {year}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {Object.keys(data)
+                .sort((a, b) => b.localeCompare(a))
+                .map(year => (
+                  <DropdownMenuItem key={year} onClick={() => setYear(year)}>
+                    {year}
+                    <DropdownMenuShortcut>
+                      &#8721;
+                      {data[year].reduce(
+                        (acc, activity) => acc + activity.count,
+                        0,
+                      )}
+                    </DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <div className="border-border relative rounded-lg border px-4 py-1 pb-2 [&_svg]:w-full">
+          <ActivityCalendar
+            data={data[year]}
+            colorScheme={isDark ? 'dark' : 'light'}
+            renderBlock={(block, activity) => {
+              if (!activity.count) return block;
+              return (
+                <Tooltip>
+                  <TooltipTrigger asChild>{block}</TooltipTrigger>
+                  <TooltipContent
+                    arrowClassName="!bg-muted-foreground !fill-muted-foreground"
+                    className="!bg-muted-foreground !text-muted-foreground-foreground rounded-lg p-2 text-center text-xs font-medium shadow-md"
+                    sideOffset={-5}
+                  >
+                    +{activity.count} new memos on{' '}
+                    {format(activity.date, 'MMM do')}
+                  </TooltipContent>
+                </Tooltip>
+              );
+            }}
+          />
+        </div>
       </div>
-      <div className="border-border relative rounded-lg border px-4 py-1 pb-2 [&_svg]:w-full">
-        <ActivityCalendar
-          data={data[year]}
-          colorScheme={isDark ? 'dark' : 'light'}
-        />
-      </div>
-    </div>
+    </TooltipProvider>
   );
 }
 
