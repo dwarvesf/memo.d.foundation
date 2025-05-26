859028c618954db299579137875cd39e9419821e
Thanh Pham <zlatan.pham@gmail.com>
2025-05-26 23:53:25 +0700

    revert: eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    ignores: ['scripts/*', 'src/types/*'],
+    ignores: ['scripts/*'],
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
