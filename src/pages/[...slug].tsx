7834c27437a611bd20500f4bde6e3eb9571ef603
Thanh Pham <zlatan.pham@gmail.com>
2025-06-02 08:34:26 +0700

    feat: add table of content (#208)
    * update the view
    
    * show/hide by toc formatter

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -472,6 +472,8 @@ export default function ContentPage({
       !frontmatter?.hide_subscription &&
       !['home', 'tags', 'contributor'].some(path => slug.includes(path));
 
+    const shouldRenderTableOfContent = frontmatter?.toc ?? false;
+
     return (
       <RootLayout
         title={frontmatter.title || 'Dwarves Memo'}
@@ -487,6 +489,7 @@ export default function ContentPage({
             title={frontmatter.title}
             description={frontmatter.description}
             backlinks={backlinks}
+            tableOfContents={shouldRenderTableOfContent ? tocItems : undefined}
             hideFrontmatter={frontmatter.hide_frontmatter}
             hideTitle={frontmatter.hide_title}
             metadata={metadata}
