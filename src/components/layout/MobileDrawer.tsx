c2f2e8927d816ba2f869c98485422ac398091713
Thanh Pham <zlatan.pham@gmail.com>
2025-06-23 16:25:01 +0700

    feat: layout tab for tree view

--- src/components/layout/MobileDrawer.tsx
+++ src/components/layout/MobileDrawer.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { Drawer, DrawerContent } from '../ui/drawer';
 import Link from 'next/link';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '../ui/tooltip';
 import { cn } from '@/lib/utils';
-import { ChevronRightIcon } from 'lucide-react';
+import { ChevronRightIcon, FolderTreeIcon, BookmarkIcon } from 'lucide-react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tab';
 
 interface NavLink {
   title: string;
@@ -38,47 +33,64 @@ const MobileDrawer = ({
   return (
     <Drawer open={isOpen} onOpenChange={setIsOpen} direction="bottom">
       <DrawerContent
-        className={cn(
-          'h-full',
-          'flex flex-col', // Added for vertical layout
-        )}
-        style={{ boxShadow: '2px 0 16px rgba(0, 0, 0, 0.08)' }}
+        hideHandle
+        className={cn('h-full', 'flex flex-col')}
         onKeyDown={handleKeyDown}
         role="navigation"
       >
         <div className="flex h-full flex-col">
-          {/* Navigation items */}
-          <nav className="flex flex-1 flex-col gap-1.5 p-4">
-            {navLinks.map((item, index) => (
-              <TooltipProvider key={item.url} skipDelayDuration={0}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Link
-                      href={item.url}
-                      className={cn(
-                        'hover:bg-muted dark:hover:bg-muted flex items-center justify-start rounded-lg text-sm font-medium transition-colors',
-                        'justify-between px-2 py-2', // Mobile specific styling
-                        {
-                          'text-primary': isActiveUrl(item.url),
-                        },
-                      )}
-                      id={`sidebar-item-${index}`}
-                      onClick={() => setIsOpen(false)}
-                    >
-                      <div className="flex items-center">
-                        {item.Icon && <item.Icon className="h-5 w-5" />}
-                        <span className="ml-3 inline-block">{item.title}</span>
-                      </div>
-                      <ChevronRightIcon className="h-4 w-4 text-gray-400" />
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent side="right" className="hidden">
-                    {item.title}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ))}
-          </nav>
+          <Tabs defaultValue="main" className="flex h-full flex-col !p-0">
+            <TabsList className="grid !h-10 w-full grid-cols-2 !overflow-hidden !rounded-none !rounded-t-xl !border-none !p-0">
+              <TabsTrigger
+                className="!h-10 !rounded-none !shadow-none"
+                value="main"
+              >
+                <BookmarkIcon className="mr-2 h-4 w-4" /> Home
+              </TabsTrigger>
+              <TabsTrigger
+                className="!h-10 !rounded-none !shadow-none"
+                value="tree"
+              >
+                <FolderTreeIcon className="mr-2 h-4 w-4" /> All pages
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="main"
+              className="flex flex-1 flex-col overflow-y-auto"
+            >
+              {/* Navigation items */}
+              <nav className="flex flex-1 flex-col gap-1.5 p-4">
+                {navLinks.map((item, index) => (
+                  <Link
+                    key={item.url}
+                    href={item.url}
+                    className={cn(
+                      'hover:bg-muted dark:hover:bg-muted flex items-center justify-start rounded-lg text-sm font-medium transition-colors',
+                      'justify-between px-2 py-2', // Mobile specific styling
+                      {
+                        'text-primary': isActiveUrl(item.url),
+                      },
+                    )}
+                    id={`sidebar-item-${index}`}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <div className="flex items-center">
+                      {item.Icon && <item.Icon className="h-5 w-5" />}
+                      <span className="ml-3 inline-block">{item.title}</span>
+                    </div>
+                    <ChevronRightIcon className="h-4 w-4 text-gray-400" />
+                  </Link>
+                ))}
+              </nav>
+            </TabsContent>
+            <TabsContent
+              value="tree"
+              className="flex flex-1 flex-col overflow-y-auto p-4"
+            >
+              {/* Tree directory content will go here */}
+              <p>Tree directory content will be added later.</p>
+            </TabsContent>
+          </Tabs>
 
           {/* Theme toggle and Close Menu */}
           <div className="mx-4 space-y-2 border-t pt-4 pb-4">
