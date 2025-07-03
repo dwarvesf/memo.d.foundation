ba295ac46ba04768951790540c7fe757bfd4b585
Thanh Pham <zlatan.pham@gmail.com>
2025-07-03 13:14:44 +0700

    fix: social icon

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useMemo } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -16,15 +18,17 @@ import {
 } from '@/components/ui/dialog';
 import {
   Share2Icon,
-  XIcon,
-  FacebookIcon,
-  LinkedinIcon,
-  LinkIcon,
   CopyIcon,
   ChevronDownIcon,
-  Printer,
+  // Printer,
+  LinkIcon,
 } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import EmailIcon from './icons/social/EmailIcon';
+import XIcon from './icons/social/XIcon';
+import FacebookIcon from './icons/social/FacebookIcon';
+import LinkedinIcon from './icons/social/LinkedinIcon';
+import { toast } from 'sonner';
 
 interface Props {
   className?: string;
@@ -32,6 +36,51 @@ interface Props {
 
 const ShareButton = (props: Props) => {
   const { className } = props;
+
+  const pageUrl = useMemo(() => {
+    if (typeof window === 'undefined') {
+      return '';
+    }
+
+    return (window?._memo_frontmatter?.redirect || [])
+      .map(link => {
+        return window.location.protocol + '//' + window.location.host + link;
+      })
+      .reduce((a, c) => {
+        if (c.length < a.length) {
+          return c;
+        }
+        return a;
+      }, window.location.href);
+  }, []);
+
+  const copyLinktoClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(pageUrl);
+      toast.success('Copied memo link!');
+    } catch (error) {
+      console.error('Failed to copy link:', error);
+    }
+  };
+
+  const copyMemoContent = async () => {
+    try {
+      const memoContent = document.querySelector(
+        '.memo-content',
+      ) as HTMLElement;
+      let content = 'No content found';
+      if (memoContent) {
+        content =
+          memoContent.textContent ||
+          memoContent.innerHTML ||
+          ''.replace(/\n\s*\n\s*\n/g, '\n\n').trim();
+      }
+      navigator.clipboard.writeText(content);
+      toast.success('Copied memo content!');
+    } catch (error) {
+      console.error('Failed to copy memo content:', error);
+    }
+  };
 
   return (
     <Dialog>
@@ -57,15 +106,23 @@ const ShareButton = (props: Props) => {
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            <DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => copyMemoContent()}
+            >
               <CopyIcon className="mr-2 h-4 w-4" />
               <span>Copy page</span>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Printer className="mr-2 h-4 w-4" />{' '}
-              {/* Using LinkIcon as a placeholder for export cheatsheet */}
-              <span>Print cheatsheet</span>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => copyLinktoClipboard()}
+            >
+              <LinkIcon className="mr-2 h-4 w-4" />
+              <span>Copy link</span>
             </DropdownMenuItem>
+            {/* <DropdownMenuItem className="cursor-pointer">
+              <Printer className="mr-2 h-4 w-4" /> <span>Print cheatsheet</span>
+            </DropdownMenuItem> */}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
@@ -77,22 +134,22 @@ const ShareButton = (props: Props) => {
             Choose how you want to share this page.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <Button variant="outline" className="justify-start">
-            <LinkIcon className="mr-2 h-4 w-4" />
-            Copy link
-          </Button>
-          <Button variant="outline" className="justify-start">
-            <XIcon className="mr-2 h-4 w-4" />
+        <div className="grid gap-3">
+          <Button variant="outline" className="h-auto justify-start">
+            <XIcon className="mr-2 !h-7 !w-7" />
             Share to X
           </Button>
-          <Button variant="outline" className="justify-start">
-            <FacebookIcon className="mr-2 h-4 w-4" />
+          <Button variant="outline" className="h-auto justify-start">
+            <FacebookIcon className="mr-2 !h-7 !w-7" />
             Share to Facebook
           </Button>
-          <Button variant="outline" className="justify-start">
-            <LinkedinIcon className="mr-2 h-4 w-4" />
+          <Button variant="outline" className="h-auto justify-start">
+            <LinkedinIcon className="mr-2 !h-7 !w-7" />
             Share to LinkedIn
+          </Button>
+          <Button variant="outline" className="h-auto justify-start">
+            <EmailIcon className="mr-2 !h-7 !w-7" />
+            Share to Email
           </Button>
         </div>
       </DialogContent>
